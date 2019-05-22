fc17a6dc2d585609e43b8a21270f011efe94fd27
utkuglsvn <utku.glsvn@gmail.com>
2019-05-22 14:42:47 +0300

    Design and Activity

--- app/src/main/java/com/example/glsvn/anirec/MainActivity.java
+++ app/src/main/java/com/example/glsvn/anirec/MainActivity.java
@@ -1,25 +1,147 @@
 package com.example.glsvn.anirec;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.InputStream;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     CircleImageView imageview;
+    ImageView imageview2;
+    private Uri imageUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageview=findViewById(R.id.imageview);
-
+        imageview = findViewById(R.id.imageview);
+        imageview2 = findViewById(R.id.imageview2);
         imageview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                showPictureDialog();
             }
         });
+    }
+
+    /*
+    public void kaydet() {
+
+        if (imageUri != null) {
+
+            final StorageReference imagePath = mStorageRef.child("images/"+"plantImg");
+
+            imagePath.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                }
+            });
+            imagePath.putFile(imageUri).continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if (!task.isSuccessful()){
+                        throw task.getException();
+                    }
+                    return imagePath.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isComplete()){
+                        Uri downUri = task.getResult();
+                        imagelink=downUri.toString();
+                        databaseReference.setValue(imagelink);
+                        getTxt();
+                      /*  try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }*/
+  /*                      Toast.makeText(getBaseContext(),"Kaydedildi.",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+*/
+
+    private void showPictureDialog() {
+        AlertDialog.Builder pictureDialog = new AlertDialog.Builder(this);
+        pictureDialog.setTitle("Seciniz");
+        String[] pictureDialogItems = {
+                "Galeriden fotağraf seçiniz",
+                "Kameradan fotağraf seçiniz"};
+        pictureDialog.setItems(pictureDialogItems,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case 0:
+                                choosePhotoFromGallary();
+                                break;
+                            case 1:
+                                takePhotoFromCamera();
+                                break;
+                        }
+                    }
+                });
+        pictureDialog.show();
+
+    }
+
+    void choosePhotoFromGallary() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, 1);
+
+    }
+
+    void takePhotoFromCamera() {
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 2);
+    }
+
+
+    @Override
+    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            switch (reqCode) {
+                case 1:
+                    try {
+                        imageUri = data.getData();
+                        final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                        final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                        imageview2.setImageBitmap(selectedImage);
+                        //txt.setVisibility(View.GONE);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 2:
+                    Bitmap photo = (Bitmap) data.getExtras().get("data");
+                    imageview2.setImageBitmap(photo);
+                    //saveImage(photo);
+                    // imageUri = data.getData();
+                    //txt.setText("Resmi Galeriden seçiniz");
+                    break;
+            }
+        }
+
     }
 }
