146399a55082c44bc695bcb9018631d47507ee30
utkuglsvn <utku.glsvn@gmail.com>
2019-05-22 15:27:23 +0300

    Firebase

--- app/src/main/java/com/example/glsvn/anirec/MainActivity.java
+++ app/src/main/java/com/example/glsvn/anirec/MainActivity.java
@@ -1,16 +1,31 @@
 package com.example.glsvn.anirec;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.InputStream;
 
@@ -20,22 +35,48 @@ public class MainActivity extends AppCompatActivity {
 
     CircleImageView imageview;
     ImageView imageview2;
+    Button save;
+
     private Uri imageUri;
+    private String imagelink;
+
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference,databaseReference2;
+    private FirebaseStorage firebaseStorage;
+    private StorageReference mStorageRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageview = findViewById(R.id.imageview);
         imageview2 = findViewById(R.id.imageview2);
+        save=findViewById(R.id.save);
+
+        firebaseDatabase=FirebaseDatabase.getInstance();
+        databaseReference=firebaseDatabase.getReference().child("Images");
+        databaseReference2=firebaseDatabase.getReference().child("results");
+        firebaseStorage=FirebaseStorage.getInstance();
+        mStorageRef=firebaseStorage.getReference();
+
+        internetControl();
+
         imageview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showPictureDialog();
             }
         });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                kaydet();
+            }
+        });
     }
 
-    /*
+
     public void kaydet() {
 
         if (imageUri != null) {
@@ -63,20 +104,16 @@ public class MainActivity extends AppCompatActivity {
                         Uri downUri = task.getResult();
                         imagelink=downUri.toString();
                         databaseReference.setValue(imagelink);
-                        getTxt();
-                      /*  try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }*/
-  /*                      Toast.makeText(getBaseContext(),"Kaydedildi.",Toast.LENGTH_SHORT).show();
+
+                        Toast.makeText(getBaseContext(),"Kaydedildi.",Toast.LENGTH_SHORT).show();
                     }
                 }
             });
         }
+
     }
 
-*/
+
 
     private void showPictureDialog() {
         AlertDialog.Builder pictureDialog = new AlertDialog.Builder(this);
@@ -129,6 +166,7 @@ public class MainActivity extends AppCompatActivity {
                         final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
                         imageview2.setImageBitmap(selectedImage);
                         //txt.setVisibility(View.GONE);
+                        save.setEnabled(true);
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
@@ -139,9 +177,22 @@ public class MainActivity extends AppCompatActivity {
                     //saveImage(photo);
                     // imageUri = data.getData();
                     //txt.setText("Resmi Galeriden seçiniz");
+                    save.setEnabled(true);
                     break;
             }
         }
 
+    }
+
+    void internetControl() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+        if (isConnected == false) {
+            Toast.makeText(getBaseContext(), "İnternet bağlantısı yok!", Toast.LENGTH_SHORT).show();
+
+        }
     }
 }
